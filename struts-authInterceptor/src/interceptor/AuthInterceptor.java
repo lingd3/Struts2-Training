2c60ab72808a91c5113a30033bd956803322af81
linqiaonihao <824928207@qq.com>
2016-10-12 09:10:54 +0800

    training

---
+++ struts-authInterceptor/src/interceptor/AuthInterceptor.java
@@ -0,0 +1,24 @@
+package interceptor;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class AuthInterceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		ActionContext context = ActionContext.getContext();
+		Map<String, Object> session = context.getSession();
+		if (session.get("loginInfo") != null) {
+			String result = invocation.invoke();
+			return result;
+		}
+		else {
+			return "login";
+		}
+	}
+
+}
